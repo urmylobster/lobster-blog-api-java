e0be3df43bd7a5cf87d157ff7107e23a265a0252
imurlobster <alexander2720@163.com>
2022-08-07 23:50:07 +0800

    chore(custom): init lobster blog api spring boot project

---
+++ src/main/java/com/lobster/tobebraverman/LobsterBlogApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lobster.tobebraverman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobsterBlogApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobsterBlogApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobster/tobebraverman/LobsterBlogApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobster.tobebraverman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobsterBlogApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
